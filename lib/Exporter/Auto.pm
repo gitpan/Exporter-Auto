a2c6f374eb29a5049e8513ba1b45073dbd88c695
Neil Bowers <neil@bowers.com>
2014-08-16 18:46:54 +0000

    Import of NEILB/Exporter-Auto-0.04 from CPAN.
    gitpan-cpan-distribution: Exporter-Auto
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         NEILB/Exporter-Auto-0.04.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/Exporter/Auto.pm
+++ lib/Exporter/Auto.pm
@@ -2,7 +2,7 @@ package Exporter::Auto;
 use strict;
 use warnings;
 use 5.008005;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Sub::Identify qw(stash_name);
 use B::Hooks::EndOfScope;
@@ -17,7 +17,7 @@ sub import {
     on_scope_end {
         my %hash = %{"${klass}::"};
         while (my ($k, $v) = each %hash) {
-            next if $k =~ /^(?:BEGIN|CHECK|END)$/;
+            next if $k =~ /^(?:BEGIN|CHECK|END|INIT|UNITCHECK)$/;
             next if $k =~ /^_/;
             next unless *{"${klass}::${k}"}{CODE};
             next if $klass ne stash_name($klass->can($k));
@@ -33,7 +33,7 @@ __END__
 
 =head1 NAME
 
-Exporter::Auto - Export public functions automatically.
+Exporter::Auto - export all public functions from your package
 
 =head1 SYNOPSIS
 
@@ -48,16 +48,72 @@ Exporter::Auto - Export public functions automatically.
 
 =head1 DESCRIPTION
 
-I'm tired to push function names to @EXPORT. Perl5 should add functions automatically!!!111
+Exporter::Auto is a simple replacement for L<Exporter> that will export
+all public functions from your package. If you want all functions to be
+exported from your module by default, then this might be the module for you.
+If you only want some functions exported, or want tags, or to export variables,
+then you should look at one of the other Exporter modules (L</"SEE ALSO">).
+
+Let's say you have a library module with three functions, all of which
+you want to export by default. With L<Exporter>, you'd write something like:
+
+    package MyLibrary;
+    use parent 'Exporter';
+    our @EXPORT = qw/ foo bar baz /;
+    sub foo { ... }
+    sub bar { ... }
+    sub baz { ... }
+    1;
+
+Every time you add a new function,
+you must remember to add it to C<@EXPORT>.
+Not a big hassle, but a small inconvenience.
+
+With C<Exporter::Auto> you just write:
+
+    package MyLibrary;
+    use Exporter::Auto;
+    sub foo { ... }
+    sub bar { ... }
+    sub baz { ... }
+    1;
+
+When you C<use Exporter::Auto> it automatically adds an C<import> function
+to your package, so you don't need to declare your package as a subclass.
+
+That's it. If you want anything more fancy than this,
+it's time for another module.
+
+=head1 REPOSITORY
+
+L<https://github.com/tokuhirom/Exporter-Auto>
 
 =head1 AUTHOR
 
 Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
 
-=head1 SEE ALSO
+=head1 DEPENDENCIES
 
 This module uses magical L<B::Hooks::EndOfScope>.
 If you think this module is too clever, please try L<Module::Functions> instead.
+
+=head1 SEE ALSO
+
+L<Exporter> is the grandaddy of all Exporter modules, and bundled with Perl
+itself, unlike the rest of the modules listed here.
+
+L<Sub::Exporter> is a "sophisticated exporter for custom-built routines";
+it lets you provide generators that can be used to customise what
+gets imported when someone uses your module.
+
+L<Exporter::Tiny> provides the same features as L<Sub::Exporter>,
+but relying only on core dependencies.
+
+L<Exporter::Declare> provides Moose-style functions used to define
+what your module exports in a declarative way.
+
+L<Exporter::Lite> is a lightweight exporter module, falling somewhere
+between C<Exporter::Auto> and L<Exporter>.
 
 =head1 LICENSE
 
